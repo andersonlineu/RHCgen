5f037d49c63e887ebbe1eb34ff5cb1746d7a4ad7
Anderson_Lineu <andersonlineu@gmail.com>
2024-07-25 23:49:47 -0300

    Frist Commit

---
+++ R/RHCgen-package.R
@@ -0,0 +1,21 @@
+#' RHCgen é uma biblioteca desenvolvida para facilitar a construção de bancos de dados a partir das informações sobre registros de câncer disponíveis no Registro Hospitalar de Câncer (RHC). 
+#'
+#' @name RHCgen-package
+#' @aliases RHCgen
+#' @docType package
+#' @title RHCgen: Pacote para construção de Bando de Dados de registros de câncer do RHC
+#'
+#' @details Este pacote oferece ferramentas para carregar e combinar arquivos DBF, realizar ajustes nas variáveis e, por fim, criar um banco de dados organizado para análise posterior. 
+#'
+#' Veja o Baixar_Arquivos_DBF para um guia passo a passo sobre como baixar os arquivos DBF no IntegradorRHC: \code{\link{Baixar_Arquivos_DBF}}
+#' @author
+#' Anderson Lineu
+#'
+#' Email: \email{andersonlineu@gmail.com}
+#'
+#' Lattes: \url{ http://lattes.cnpq.br/9727490230028203}
+#'
+#' Orcid iD: \url{ https://orcid.org/0000-0002-1703-9310}
+#' @keywords package
+#' @seealso \code{\link{Baixar_Arquivos_DBF}}
+"_PACKAGE"
